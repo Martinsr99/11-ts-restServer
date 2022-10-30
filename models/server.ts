33d7a43e1a7ce8f6bb0905827382c22aa1a4ebcb
Martinsr99 <53481948+Martinsr99@users.noreply.github.com>
2022-10-30 13:38:08 +0100

    Install dependencies and create first version

---
+++ models/server.ts
@@ -0,0 +1,21 @@
+import express from 'express'
+
+
+class Server{
+
+    private app: express.Application;
+    private port: string;
+
+    constructor(){
+        this.app = express();
+        this.port = process.env.PORT ||'8080'
+    }
+
+    listen(){
+        this.app.listen(this.port,() => {
+            console.log('Servidor corriendo en puerto ' + this.port)
+        })
+    }
+}
+
+export default Server
